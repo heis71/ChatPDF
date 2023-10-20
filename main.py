c54537438f9bf0f0231db1c4c105e7b72f2a4ccb
Jonathan <smzk7i@gmail.com>
2023-10-20 16:04:14 +0800

    refactor: update project chat logic

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
-from langchain.document_loaders import PyPDFLoader
-from langchain.vectorstores import Chroma
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.document_loaders import UnstructuredPDFLoader
-# from langchain.llms import OpenAI
-from langchain.chat_models import ChatOpenAI
+import os
+import time
+
 from langchain.chains.question_answering import load_qa_chain
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import UnstructuredPDFLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
 
-# Replace book.pdf with any pdf of your choice
-loader = UnstructuredPDFLoader("book.pdf")
-pages = loader.load_and_split()
-embeddings = OpenAIEmbeddings()
-docsearch = Chroma.from_documents(pages, embeddings).as_retriever()
 
-# Choose any query of your choice
-query = "Who is Rich Dad?"
-docs = docsearch.get_relevant_documents(query)
-# chain = load_qa_chain(OpenAI(temperature=0), chain_type="stuff")
-chain = load_qa_chain(ChatOpenAI(temperature=0), chain_type="stuff")
-output = chain.run(input_documents=docs, question=query)
-print(output)
+def main():
+    # Loader file from environment variable
+    loader = UnstructuredPDFLoader(os.getenv("PDF_FILE_PATH", "book.pdf"))
+    pages = loader.load_and_split()
+    embeddings = OpenAIEmbeddings()
+    docsearch = Chroma.from_documents(pages, embeddings).as_retriever()
+    while True:
+        query = input("Please enter your question (type 'exit' to quit):").strip()
+        if query == "exit":
+            break
+
+        print('Processing, please wait...')
+        start_time = time.time()
+        docs = docsearch.get_relevant_documents(query)
+        chain = load_qa_chain(ChatOpenAI(temperature=0), chain_type="stuff")
+        output = chain.run(input_documents=docs, question=query)
+        elapsed_time = time.time() - start_time
+
+        print(output)
+        print(f"Processing complete, duration：{elapsed_time:.2f}s\n")
+
+
+if __name__ == '__main__':
+    main()
